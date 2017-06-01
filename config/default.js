5c4ed9ed9d3a79eafc06c3d3bc2414541388b3c7
freebreaker <592378845@qq.com>
2017-06-01 09:46:10 +0800

    production.js

--- config/default.js
+++ config/default.js
@@ -6,6 +6,4 @@ module.exports = {
     maxAge: 2592000000
   },
   mongodb: 'mongodb://localhost:27017/myblog'
-  //   port: 80,
-  // mongodb: 'mongodb://freebrealer:freebrealer@ds149201.mlab.com:49201/myblog'
 };
---
+++ config/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: 80,
+  mongodb: 'mongodb://freebreaker:freebreaker@ds149201.mlab.com:49201/myblog'
+};
